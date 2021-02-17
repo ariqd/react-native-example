af5884ea7147d005b23e20d624ecfe167619beef
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-02-17 16:49:26 +0700

    feature 1 ready

---
+++ components/Greeting.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const Greeting = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Write your name"
+        onChangeText={(text) => setName(text)}
+      />
+      <Text style={{ marginTop: 8 }}>
+        Welcome to this CI/CD Course, {name}!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    marginHorizontal: 16,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+  },
+  input: {
+    height: 30,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+});
+
+export default Greeting;
